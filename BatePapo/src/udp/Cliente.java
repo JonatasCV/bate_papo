c92960c3a768025d7703f7f403c611a0cd94bb23
Rafael Vissotto <rafaelvissotto@gmail.com>
2018-06-02 23:59:57 -0300

    algumas modificacoes no udp

--- BatePapo/src/udp/Cliente.java
+++ BatePapo/src/udp/Cliente.java
@@ -272,6 +272,7 @@ public class Cliente extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new Cliente().setVisible(true);
             }
