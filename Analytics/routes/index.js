0eca3e3ef9439c9ff4cceec94578ace3ea090532
GabeConsalter <consaltergabriel@gmail.com>
2018-06-04 15:14:08 -0300

    Request

--- Analytics/routes/index.js
+++ Analytics/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   res.render('index', { title: 'Express' });
+});
+
+router.post('/chats', (req, res) => {
+
+  request.get({
+      uri: 'https://batepaporest.herokuapp.com/chats-realizados',
+      form: {filter: ''}
+    },
+    (err, requestRes, body) => {
+      res.send(JSON.parse(body));
+    });
 });
 
 module.exports = router;
